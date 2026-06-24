851c844314ad613f62268fa4738955ee00213a2e
DevExploreDot <senauluquipas@gmail.com>
2026-06-24 14:02:21 -0400

    feat: Inicializar los archivos de configuración del entorno para los modos de producción y desarrollo.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://urbancontrol-api-io2l.onrender.com/api/v1'
+  // apiUrl: 'https://urbancontrol-api-io2l.onrender.com/api/v1' // Producción (Render)
+  apiUrl: 'https://urbancontrol.onrender.com/api/v1' // Desarrollo (Render)
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '/api/v1' // El proxy de Angular redirige esto a urbancontrol.onrender.com
+  apiUrl: '/api/v1' // Desarrollo (El proxy de Angular redirige esto a la URL local o de pruebas)
+  // apiUrl: 'https://urbancontrol-api-io2l.onrender.com/api/v1' // Producción (Render)
 };
